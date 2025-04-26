809bf01d6a7922a01ca84c8d6a3bddc956801be8
StefanNeedHelp <stefan.needhelp2025@gmail.com>
2025-04-26 22:20:02 +0200

    Add scrape_jobs.pyfile

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -1,12 +1,18 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 
-options = uc.ChromeOptions()
-options.add_argument("--headless")
+# Postavi tačnu lokalnu putanju do chromedriver.exe
+CHROMEDRIVER_PATH = "C:/chromedriver/chromedriver.exe"  # prilagodi ako je na drugoj lokaciji
+
+options = Options()
+options.add_argument("--headless")  # Ne prikazuje prozor
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options, use_subprocess=True)
+service = Service(CHROMEDRIVER_PATH)
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.get("https://example.com")
