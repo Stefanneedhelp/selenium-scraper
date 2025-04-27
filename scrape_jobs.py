39e42285f93dbe90574d0680a5248271a34a41ef
StefanNeedHelp <stefan.needhelp2025@gmail.com>
2025-04-27 07:50:31 +0200

    Update scrape_jobs.pyto fix binary location issue

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -1,12 +1,22 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
+import os
 
-options = uc.ChromeOptions()
-options.add_argument("--headless")
+# ChromeDriver putanja koju Render koristi
+CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+GOOGLE_CHROME_BIN = "/usr/bin/google-chrome"
+
+options = Options()
+options.binary_location = GOOGLE_CHROME_BIN
+options.add_argument("--headless")  # Ne prikazuje prozor
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options, use_subprocess=True)
+service = Service(CHROMEDRIVER_PATH)
+
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
     driver.get("https://example.com")
